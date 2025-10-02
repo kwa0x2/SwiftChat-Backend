3002a59853d118235043761ea1157b8483954ba3
Alper Karakoyun <alperenibo14@gmail.com>
2025-10-02 22:00:38 +0100

    feat: cors env

--- config/env.go
+++ config/env.go
@@ -25,6 +25,7 @@ type Config struct {
 	SessionSecretKey    string `mapstructure:"SESSION_SECRET_KEY" validate:"required"`
 	ResendAPIKey        string `mapstructure:"RESEND_API_KEY" validate:"required"`
 	JWTSecretKey        string `mapstructure:"JWT_SECRET_KEY" validate:"required"`
+	CorsURL 		   string `mapstructure:"CORS_URL" validate:"required"`
 }
 
 var Env Config
